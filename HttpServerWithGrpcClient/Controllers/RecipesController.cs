75a7ac12f459d156cb05aad9a8641e795fc30baa
Carlos341988 <magulon26@gmail.com>
2023-09-12 21:00:56 -0300

    like dislike and create

--- HttpServerWithGrpcClient/Controllers/RecipesController.cs
+++ HttpServerWithGrpcClient/Controllers/RecipesController.cs
@@ -45,6 +45,110 @@ namespace HttpServerWithGrpcClient.Controllers
     [ApiController]
     public class RecipesController : ControllerBase
     {
+    [HttpPost("favorites/{recipe_id}")]
+        public HttpServerWothGrpcClient.Response likeRecipe(int recipe_id, [FromBody] int idUser)
+        {
+            try
+            {
+                Debug.WriteLine("like");
+                Debug.WriteLine("id");
+                Debug.WriteLine($"{recipe_id}");
+                Debug.WriteLine("user");
+                Debug.WriteLine($"{idUser}");
+                using var channel = GrpcChannel.ForAddress("http://localhost:50051/");
+                Debug.WriteLine(1);
+                var client = new ChefEnCasa.ChefEnCasaClient(channel);
+                Debug.WriteLine(2);
+                var body = new HttpServerWothGrpcClient.RequestReciepeToFavorites
+                {
+                    IdReciepe = recipe_id,
+                    IdUser = idUser
+                };
+
+                Debug.WriteLine(3);
+
+                return client.AddReciepeToFavorites(body);
+            } catch (Exception ex)
+            {
+                Debug.WriteLine("---------------------------");
+                Debug.WriteLine(ex.ToString());
+                
+
+
+                throw new Exception();
+            }
+        }
+
+        [HttpDelete("favorites/{recipe_id}")]
+        public HttpServerWothGrpcClient.Response dislikeRecipe(int recipe_id, [FromBody] int idUser)
+        {
+            Debug.WriteLine("dislike");
+            Debug.WriteLine("id");
+            Debug.WriteLine($"{recipe_id}");
+            Debug.WriteLine("user");
+            Debug.WriteLine($"{idUser}");
+            using var channel = GrpcChannel.ForAddress("http://localhost:50051/");
+
+            var client = new ChefEnCasa.ChefEnCasaClient(channel);
+
+            var body = new HttpServerWothGrpcClient.RequestReciepeToFavorites
+            {
+                IdReciepe = recipe_id,
+                IdUser = idUser
+            };
+
+            return client.RemoveReciepeToFavorites(body);
+        }
+        // POST api/<RecipesController>
+            [HttpPost]
+public HttpServerWothGrpcClient.Response Post([FromBody] RecipeData reciepeData)
+         {
+           Debug.WriteLine("debug information");
+
+           using var channel = GrpcChannel.ForAddress("http://localhost:50051", new GrpcChannelOptions
+           {
+               Credentials = Grpc.Core.ChannelCredentials.Insecure // You might need to replace this with secure credentials
+           });
+
+           var client = new ChefEnCasa.ChefEnCasaClient(channel);
+           var request = new HttpServerWothGrpcClient.RequestCreateReciepe();
+           request.Title = reciepeData.Title;
+           request.Description = reciepeData.Description;
+           request.PrepatarionTimeMinutes = reciepeData.PreparationTimeMinutes;
+           var category = new HttpServerWothGrpcClient.Category();
+           category.Id = reciepeData.IdCategory;
+           request.Category = category;
+
+           foreach (PhotoData photo in reciepeData.Photos)
+           {
+               var p = new HttpServerWothGrpcClient.Photo();
+               p.Id = photo.Id;
+               p.Url = photo.Url;
+               request.Photos.Add(p);
+           }
+
+           foreach (SteptData stept in reciepeData.Stepts)
+           {
+               var s = new HttpServerWothGrpcClient.Stept();
+               s.Id = stept.Id;
+               s.Description = stept.Description;
+               request.Stepts.Add(s);
+           }
+
+           foreach (IngredientData ingredient in reciepeData.Ingredients)
+           {
+               var i = new HttpServerWothGrpcClient.Ingredient();
+               i.Id = ingredient.Id;
+               i.Name = ingredient.Name;
+               request.Ingredients.Add(i);
+           }
+
+
+           var reply = client.CreateRecipe(request);
+
+           return reply;
+         }
+
         // GET: api/<RecipesController>
         [HttpGet]
         public HttpServerWothGrpcClient.ResponseRecipies Get([FromQuery] string title = ".", [FromQuery] int prepTime = 120, [FromQuery] int categoryId = 0)
