785602047f8ebdd24a1b2dbb8ca3eefc7bc0c174
Mauro Lucas <30880379+MauroLucas@users.noreply.github.com>
2023-09-14 08:52:14 -0300

    Creación FollowUser y UnFollowUser

---
+++ HttpServerWithGrpcClient/Controllers/FollowController.cs
@@ -0,0 +1,53 @@
+﻿using Grpc.Net.Client;
+using HttpServerWothGrpcClient;
+using Microsoft.AspNetCore.Mvc;
+
+public class RequestFollowData
+{
+    public int IdUser { get; set; }
+    public int IdChefUser { get; set; }
+}
+
+namespace HttpServerWithGrpcClient.Controllers
+{
+    [Route("api/follow")]
+    [ApiController]
+    public class FollowController : Controller
+    {
+        [HttpPost]
+        public HttpServerWothGrpcClient.Response Post([FromBody] RequestFollowData data)
+        {
+            using var channel = GrpcChannel.ForAddress("http://localhost:50051", new GrpcChannelOptions
+            {
+                Credentials = Grpc.Core.ChannelCredentials.Insecure // You might need to replace this with secure credentials
+            });
+
+            var client = new ChefEnCasa.ChefEnCasaClient(channel);
+
+            var request = new HttpServerWothGrpcClient.RequestFollowUser();
+
+            request.IdUser = data.IdUser;
+            request.IdChefUser = data.IdChefUser;
+
+            return client.FollowUser(request);
+        }
+
+        [HttpDelete]
+        public HttpServerWothGrpcClient.Response Delete([FromBody] RequestFollowData data)
+        {
+            using var channel = GrpcChannel.ForAddress("http://localhost:50051", new GrpcChannelOptions
+            {
+                Credentials = Grpc.Core.ChannelCredentials.Insecure // You might need to replace this with secure credentials
+            });
+
+            var client = new ChefEnCasa.ChefEnCasaClient(channel);
+
+            var request = new HttpServerWothGrpcClient.RequestFollowUser();
+
+            request.IdUser = data.IdUser;
+            request.IdChefUser = data.IdChefUser;
+
+            return client.UnFollowUser(request);
+        }
+    }
+}
